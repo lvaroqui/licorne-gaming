0deb13fad677865e081aa9f26d8e2f1340c06f01
Luc Varoqui <luc@varoqui.org>
2023-08-06 12:53:00 +0200

    backend hello world

--- front/src/app/login.rs
+++ front/src/app/login.rs
@@ -14,7 +14,7 @@ pub fn Login(cx: Scope) -> impl IntoView {
     };
     view! { cx,
         <div class="flex items-center justify-center w-full h-full">
-            <div class="flex flex-col w-full max-w-sm p-3 m-4 border-2 border-gray-900 shadow-xl bg-slate-700 max-sm:container rounded-xl">
+            <div class="max-sm:container flex flex-col w-full max-w-sm p-3 m-4 border-2 border-gray-900 shadow-xl bg-slate-700 rounded-xl">
                 <h1 class="mb-2 text-3xl font-bold text-white">Login</h1>
                 <TextInput value=login placeholder="Login" />
                 <TextInput value=password placeholder="Password" password=true />
