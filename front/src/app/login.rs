faaede70af10b769d7a6cdc8722e976844945923
Luc Varoqui <luc@varoqui.org>
2023-08-08 00:39:57 +0200

    infra: improve docker containers startup and caching

--- front/src/app/login.rs
+++ front/src/app/login.rs
@@ -32,9 +32,9 @@ pub fn Login(cx: Scope) -> impl IntoView {
         <div class="flex items-center justify-center w-full h-full">
             <div class="max-sm:container flex flex-col w-full max-w-sm p-3 m-4 border-2 border-gray-900 shadow-xl bg-slate-700 rounded-xl">
                 <h1 class="mb-2 text-3xl font-bold text-white">Login</h1>
-                <TextInput value=login placeholder="Logian" />
+                <TextInput value=login placeholder="Login" />
                 <TextInput value=password placeholder="Password" password=true />
-                <Button on:click=go_to_home>Login</Button>
+                <Button on:click=go_to_home>Go Back to Home</Button>
 
                 <Transition
                     fallback=move || view! { cx, <p>"Loading..."</p> }
